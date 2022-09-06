4648ca87eeeff76f61782d346f86e44bb2c0e6ca
Svastik Sharma <svastik@my.yorku.ca>
2022-09-06 01:32:30 -0400

    update2

--- jscript.js
+++ jscript.js
@@ -13,8 +13,10 @@ toggleButton.addEventListener('click', () => {
 const url = "https://api.themoviedb.org/3/"
 const key = "api_key=e062837d92b7cad745d5a14e61157a1e"
 const imageurl = "https://image.tmdb.org/t/p/w500";
-const main_url = url + "discover/movie?sort_by=popularity.desc&" + key;
-const searchurl = url + "search/movie/?" + key + "&language=en-US&page=1&include_adult=false";
+const main_url = url + "discover/movie?sort_by=popularity.desc&" + key + "&append_to_response=videos";
+const searchurl = url + "search/multi?" + key + "&language=en-US&page=1&include_adult=false";
+
+
 
 const main = document.getElementById("main");
 const search = document.getElementById("search");
@@ -29,18 +31,7 @@ const storage = Object.keys(localStorage)
 
 let addwatch = document.querySelectorAll('.addtowishlist')
 
-for(let i=0; i<addwatch.length; i++){
-    addwatch[i].addEventListener('click', () => {
-        watchNumbers();
-    })
-}
-
-function watchNumbers(){
-    let movieNumbers = localStorage.getItem('watchNumbers');
-
-    movieNumbers = parseInt(movieNumbers);
-    localStorage.setItem('watchNumbers', 1);
-}
+//  
 
 getMovies(main_url);
 
@@ -59,7 +50,7 @@ function showMovies(data) {
 
     data.forEach(movie => {
 
-        const {id, title, poster_path, overview} = movie;
+        const {id, title, original_title, original_name, name, poster_path, overview, vote_average} = movie;
 
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie_temp');
@@ -68,18 +59,33 @@ function showMovies(data) {
         <img src = "${imageurl + poster_path}" alt="Poster">
 
         <div class="title">
-            <h1> ${title}
+            <h1> 
+            ${title}
             </h1>
+        </div>
+
+        <div class = "rating_main">
+
+        <img src="Images/star.png" class="rate_img">
+
+        <div class = "rating">
+        
+        ${vote_average} / 10
+        <br> <br>
+        </div>
         </div>
 
         <div class="summary">
             ${overview}
         </div>
-
-        <a class="addtowishlist" href="#!" onclick="addwatchlist(${id})">Add to wishlist</a>
         `
+        //<a class="addtowishlist" href="#!" onclick="addwatchlist(${id})">Add to wishlist</a>
         main.appendChild(movieEl);
     })
+}
+
+function changeColour(){
+
 }
 
 /* <button class="addwatchlist" id="addwatchbutton" onclick="">Add to watchlist</button>
@@ -96,6 +102,7 @@ e.preventDefault();
 
     if(searchword){
         getMovies(searchurl + query);
+        console.log(getMovies(searchurl + query));
     }
 
     else{
@@ -106,25 +113,32 @@ e.preventDefault();
 let cart = JSON.parse(localStorage.getItem("CART")) || [];
 updateCart();
 
+const find = "https://api.themoviedb.org/3/movie/";
+const find2 = "?api_key=e062837d92b7cad745d5a14e61157a1e&language=en-US";
+
 function addwatchlist(id){
     //storage.setItem(id.innerHTML)
-    
-    fetch(main_url).then(res => res.json()).then(data => {
+    //const idinfo = 1;
+    // let idinfo = id;
+    // let mainlink = find + idinfo + find2;
+
+        fetch(infoo).then(res => res.json()).then(data => {
         //item exists or not
         if(cart.some((item) => item.id === id)){
             alert("Movie is already in the hitlist!")
         }
+        else{
         const item = data.results.find((movie) => movie.id === id);
         
         cart.push(item);
         console.log(cart);
-    })
-
+        }
+    });
     updateCart();
-
 }
 
 function updateCart(){
+    
     showList();
 
     localStorage.setItem("CART", JSON.stringify(cart));
@@ -136,26 +150,31 @@ function showList(){
 
     cart.forEach((wishlist) => {
 
-        const {title, poster_path, overview} = wishlist;
+        const {id, title, original_title, original_name, name, poster_path, overview} = wishlist;
 
         itemsEl.innerHTML += `
+
+        <div class="list_temp">
 
         <img src = "${imageurl + poster_path}" alt="image">
 
         <div class="title">
-            <h1>${title}</h1>
+            <h1>${title + name}</h1>
         </div>
         <div class="summary">
             ${overview}
         </div>
-        <a class="removefromwishlist" href="#!">Remove from wishlist</a>
+        <a class="removefromwishlist" href="#!" onclick="remove(${id})">Remove from wishlist</a>
+        </div>
         `
         main.appendChild(itemsEl);
     }
 )}
 
-function remove(title, poster_path, overview){
+function remove(id){
 
-    storage.removeItem(title, poster_path, overview)
+    cart = cart.filter( (item) => item.id !== id)
+
+    updateCart();
 }
 //END
